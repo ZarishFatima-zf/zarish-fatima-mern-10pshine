f79d058a71b5fc7cd8b97b7e4bda8a0e640b2ccf
Zarish Fatima <zarishfatima6087@gmail.com>
2025-09-26 17:19:12 +0500

    Adding Forgot-Password

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,21 +2,21 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-
+import ResetPasswordPage from "./components/ForgetPassword";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />       
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-       </Routes>
+       <Route path="/forgot-password" element={<ResetPasswordPage />} />
+        <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
+      </Routes>
     </Router>
   );
 }
 
 export default App;
-
-
 
