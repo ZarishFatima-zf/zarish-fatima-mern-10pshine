61ce978e3bc62648ea1ba84fe2ff8f60fd9aef19
Zarish Fatima <zarishfatima6087@gmail.com>
2025-10-03 15:03:44 +0500

    Updating Backend Logic

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const { registerUser } = require("../controllers/signupController");
+const { loginUser } = require("../controllers/loginController");
+const { forgotPassword, resetPassword } = require("../controllers/forgot-resetController");
+
+router.post("/signup", registerUser);
+router.post("/login", loginUser);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:token", resetPassword);
+
+module.exports = router;
